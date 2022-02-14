f812a1e81bde6cd9b8a999394374c8da71630e51
Itiel Luque <itiel.luque@movetia.com>
2022-02-14 20:04:40 +0100

    Renamed view model to be more concordant to app's theme and communication with view done.

--- Practice exercise/Practice exercise/View/ViewController.swift
+++ Practice exercise/Practice exercise/View/ViewController.swift
@@ -7,17 +7,70 @@
 
 import UIKit
 
+protocol MainViewControllerInput {
+    func fetchData()
+    func requestData(for: String)
+}
+protocol MainViewControllerOutput {
+    func getData(data: [String])
+    func requestedData(summary: ItemSummaryModel)
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var indicator: UIActivityIndicatorView!
+    
+    let vm = TransactionsViewModel()
+    lazy var delegate: MainViewControllerInput = vm
+    var data: [String] = []
+    private var product = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        let vm = OperationsViewModel()
-        
-        vm.fetchData()
+        tableView.delegate = self
+        tableView.dataSource = self
+        vm.delegate = self
+        delegate.fetchData()
+        indicator.hidesWhenStopped = true
     }
+}
 
+// MARK: MainViewControllerOutput
+extension ViewController: MainViewControllerOutput {
+    
+    func getData(data: [String]) {
+        self.data = data
+        tableView.reloadData()
+        indicator.stopAnimating()
+    }
+    
+    func requestedData(summary: ItemSummaryModel) {
+    }
+}
 
+// MARK: UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel!.text = data[indexPath.row]
+        
+        return cell
+    }
+}
+
+// MARK: UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        product = data[indexPath.row]
+        delegate.requestData(for: product)
+    }
 }
 
---
+++ Practice exercise/Practice exercise/ViewModel/TransactionsViewModel.swift
@@ -0,0 +1,106 @@
+//
+//  TransactionsViewModel.swift
+//  Practice exercise
+//
+//  Created by Itiel Luque on 13/2/22.
+//
+
+import Foundation
+import SENetworking
+
+class TransactionsViewModel: BaseViewModel {
+    var delegate: MainViewControllerOutput?
+    var transactions: [String: [TransactionResponse]] = [:]
+    var rates: [String:[RateResponse]] = [:]
+    
+    override init() {
+        super.init()
+    }
+    
+    override func baseURL() -> String {
+        return "https://quiet-stone-2094.herokuapp.com"
+    }
+    
+    func fetchRates(completion: @escaping ([RateResponse]?)->Void) {
+        let endpoint = APIEndpoints.getRates(with: nil)
+        dataTransferService().request(with: endpoint) { result in
+            guard case let .success(response) = result, let res = response else { return }
+            completion(res)
+        }
+    }
+    
+    func fetchTransactions(completion: @escaping ([TransactionResponse]?)->Void) {
+        let endpoint = APIEndpoints.getTransanctions(with: nil)
+        apiDataTransferService.request(with: endpoint) { result in
+            guard case let .success(response) = result, let res = response else { return }
+            completion(res)
+        }
+    }
+}
+
+// MARK: MainViewControllerInput
+extension TransactionsViewModel: MainViewControllerInput {
+    
+    func requestData(for selected: String) {
+        if let skuTransactions = transactions[selected] {
+            var amount: Float = 0.0
+            var currencyPairsTransactions: [FromToCurrenciesPair] = []
+            for transaction in skuTransactions {
+                let from = CurrencyModel(currency: transaction.currency, amount: Float(transaction.amount) ?? 0.0)
+                let converted = CurrencyConversor.convert(from: from, to: "EUR", rates: rates)
+                let to = CurrencyModel(currency: "EUR", amount: converted.amount)
+                
+                currencyPairsTransactions.append(FromToCurrenciesPair(from: from, to: to))
+                
+                amount = CurrencyConversor.sum(a: amount, b: converted.amount)
+            }
+            let summary = ItemSummaryModel(transactions: currencyPairsTransactions, amount: amount)
+            delegate?.requestedData(summary: summary)
+        }
+    }
+    
+    func fetchData() {
+        fetchTransactions() { data in
+            if let data = data {
+                for transaction in data {
+                    if self.transactions[transaction.sku] == nil {
+                        self.transactions[transaction.sku] = []
+                    }
+                    self.transactions[transaction.sku]?.append(transaction)
+                }
+                self.delegate?.getData(data: [String](self.transactions.keys))
+            }
+        }
+        
+        fetchRates() { data in
+            if let data = data {
+                for rate in data {
+                    if self.rates[rate.from] == nil {
+                        self.rates[rate.from] = []
+                    }
+                    self.rates[rate.from]?.append(rate)
+                }
+            }
+        }
+    }
+}
+
+// MARK: APIEndpoints
+fileprivate class APIEndpoints {
+    
+    static func getRates(with rateRequest: RateRequest?) -> Endpoint<[RateResponse]?> {
+            return Endpoint(path: "rates",
+                            method: .get,
+                            headerParamaters: ["Content-Type": "application/json",
+                                               "Accept": "application/json"],
+                            queryParametersEncodable: rateRequest)
+    }
+    
+    static func getTransanctions(with transactionRequest: TransactionRequest?) -> Endpoint<[TransactionResponse]?> {
+            return Endpoint(path: "transactions",
+                            method: .get,
+                            headerParamaters: ["Content-Type": "application/json",
+                                               "Accept": "application/json"],
+                            queryParametersEncodable: transactionRequest)
+    }
+}
